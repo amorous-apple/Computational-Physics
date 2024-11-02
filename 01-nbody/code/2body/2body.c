c6a85626b457ac23340a18e447045fd6c831b4a6
CongLong <thdragonmaster@gmail.com>
2024-11-02 18:44:36 +0100

    Changed gitignore.

---
+++ 01-nbody/code/2body/2body.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+typedef struct {
+    double pos_x;
+    double pos_y;
+    double pos_z;
+    double vel_x;
+    double vel_y;
+    double vel_z;
+    double mass;
+} Particle;
+
+int main(void) {
+    printf("Test.\n");
+    printf("Test.\n");
+}
