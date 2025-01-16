c2d236150b8c9bc71f77d970eba18c682c67a1e7
CongLong <thdragonmaster@gmail.com>
2025-01-16 15:45:53 +0100

    Added intersection testing

--- 06-stiff_rods/code/lib/utils_field.h
+++ 06-stiff_rods/code/lib/utils_field.h
@@ -3,8 +3,11 @@
 
 #include "init.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void printField(int **field);
+int testRod(Position rodPosition, char rodType, int **field);
+void placeRod(Position rodPosition, char rodType, int **field);
 void fillField(Position *rodsH, int numH, Position *rodsV, int numV,
                int **field);
 
--- 06-stiff_rods/code/main.c
+++ 06-stiff_rods/code/main.c
@@ -5,28 +5,40 @@
 #include "stdlib.h"
 
 int main() {
-    init();
+    // init();
 
     int MAX_RODS = (SYSTEM_SIZE * SYSTEM_SIZE / ROD_SIZE) + 1;
 
     Position *rodsH = malloc(MAX_RODS * sizeof(Position));
+    if (rodsH == NULL) {
+        perror("Error allocating memory for rodsH\n");
+        exit(EXIT_FAILURE);
+    }
     Position *rodsV = malloc(MAX_RODS * sizeof(Position));
+    if (rodsV == NULL) {
+        perror("Error allocating memory for rodsV\n");
+        exit(EXIT_FAILURE);
+    }
 
     int numH = 0;
     int numV = 0;
 
-    int countTotal = numH + numV;
+    // int countTotal = numH + numV;
 
     rodsH[0].posX = 0;
     rodsH[0].posY = 0;
-    numH = 1;
+    rodsH[1].posX = 2;
+    rodsH[1].posY = 4;
+    numH = 2;
 
-    rodsV[0].posX = 4;
-    rodsV[0].posX = 4;
-    numV = 1;
+    rodsV[0].posX = 3;
+    rodsV[0].posY = 0;
+    rodsV[1].posX = 4;
+    rodsV[1].posY = 3;
+    numV = 2;
 
     int **occupancyField = init_occupancyField();
-    printField(occupancyField);
+    // printField(occupancyField);
 
     printf("Initial field\n");
     fillField(rodsH, numH, rodsV, numV, occupancyField);
