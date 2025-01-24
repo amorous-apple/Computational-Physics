3a6ebbead96893e5d32ff1b2c50f27ddce2c4caa
nmatera <noah.matera@student.uni-tuebingen.de>
2025-01-24 13:54:18 +0100

    new rump it hermite

---
+++ 01-nbody/code/simulation/lib/integrators/int_hermite_it.h
@@ -0,0 +1,9 @@
+#ifndef INT_HERMITE_it_H
+#define INT_HERMITE_it_H
+
+#include "./../constants.h"
+#include "./../calc_accel.h"
+
+void calc_hermite_it(Particle* Collection1, Particle* Collection2);
+
+#endif
