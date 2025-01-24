eb409f624bd7d59bad25ff977180b73d7a20f0c7
nmatera <noah.matera@student.uni-tuebingen.de>
2025-01-24 17:15:41 +0100

    added plotter.py

---
+++ 06-stiff_rods/code/data/plotter.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+print(os.getcwd()) #tells you in working directory you are in case you need to change read_csv and savefig
+
+headers = ['t','N', 'N1', 'N2'] 
+data = pd.read_csv("./code/data/plot1.csv",names=headers, skiprows=0, skipfooter=0, engine='python')
+
+df = pd.DataFrame(data)
+
+df.plot(x='t', xlabel="xlabel", ylabel="ylabel", title="title")
+plt.savefig("./code/data/plot1.png")
