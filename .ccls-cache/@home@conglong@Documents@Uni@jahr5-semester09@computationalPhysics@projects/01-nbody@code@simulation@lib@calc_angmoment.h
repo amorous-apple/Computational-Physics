c5293be441f7f7459265c02f0dec735c343655a7
amorous-apple <n/a>
2025-10-24 17:44:23 +0200

    Stopped tracking of ccls-cache

--- .ccls-cache/@home@conglong@Documents@Uni@jahr5-semester09@computationalPhysics@projects/01-nbody@code@simulation@lib@calc_angmoment.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALC_ANGMOMENT_H
-#define CALC_ANGMOMENT_H
-
-#include "constants.h"
-#include "utils_vec.h"
-
-Vector calc_angmoment(Particle* Collection);
-
-#endif
--- .ccls-cache/@home@conglong@Documents@Uni@jahr5-semester09@computationalPhysics@projects/01-nbody@code@simulation@lib@integrators@int_middle.h
+++
@@ -1,9 +0,0 @@
-#ifndef INT_MIDDLE_H
-#define INT_MIDDLE_H
-
-#include "./../constants.h"
-#include "./../calc_accel.h"
-
-void calc_middle(Particle* Collection1, Particle* Collection2);
-
-#endif
