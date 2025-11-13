dbc59c1afe47377342a2dd9a4e39e38a907e2276
amorous-apple <n/a>
2025-11-13 22:12:19 +0100

    Initial Ising-Model commit

---
+++ 04-ising-model/code/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "C development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      myShell = "${pkgs.zsh}/bin/zsh"; # change this to bash, fish, etc.
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          zsh # shell for running the script
+          gcc # compiler
+          gnumake # add make
+          gdb # stepping through code
+          gef # extension of gdb
+          valgrind # memory debugger
+          bear # generated a compilation database
+
+          python3 # for plotting
+          python312Packages.pandas
+          python312Packages.numpy
+          python312Packages.matplotlib
+          python312Packages.imageio
+        ];
+
+        shellHook = ''
+          echo "Welcome to your C development shell with python3 plotting!"
+          exec ${myShell}
+        '';
+      };
+    };
+}
---
+++ 04-ising-model/report.typ
@@ -0,0 +1,15 @@
+#import "@preview/elsearticle:1.0.0": *
+#import "@preview/physica:0.9.6": *
+
+#show: elsearticle.with(
+  title: "Computational Physics Assignment 1: N-Body Simulation", authors: (
+    (
+      name: "R. Abele", affiliation: "Eberhard Karls University of Tuebingen",
+    ), (name: "N. Matera"),
+  ), abstract: "The following exercises showcase several integrators to solve the n-body problem. We were given initial conditions in the form of particle positions and velocities. ", numcol: 1,
+)
+
+#outline()
+
+= Background
+
