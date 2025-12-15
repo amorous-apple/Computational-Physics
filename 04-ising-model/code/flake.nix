0c43886f5447dd4807b2b26a308fd76c64fe8085
amorous-apple <n/a>
2025-12-15 10:34:13 +0100

    Updated flake to Rust

--- 04-ising-model/code/flake.nix
+++ 04-ising-model/code/flake.nix
@@ -1,36 +1,50 @@
 {
-  description = "C development environment";
+  description = "Basic Rust Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      myShell = "${pkgs.zsh}/bin/zsh"; # change this to bash, fish, etc.
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackages naersk { };
+      myShell = "${pkgs.zsh}/bin/zsh";
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          zsh # shell for running the script
-          gcc # compiler
-          gnumake # add make
-          gdb # stepping through code
-          gef # extension of gdb
-          valgrind # memory debugger
-          bear # generated a compilation database
+      packages."x86_64-linux".default = pkgs.callPackage ./default.nix { naerskLib = naerskLib; };
 
-          python3 # for plotting
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        # TOOLS: Python and plotting libraries
+        # We use 'packages' here because these are utilities to run,
+        # not libraries your Rust code needs to compile/link against.
+        packages = with pkgs; [
+          python3
           python312Packages.pandas
           python312Packages.numpy
           python312Packages.matplotlib
           python312Packages.imageio
         ];
 
+        # LIBRARIES & BUILD DEPS: Rust compilation needs
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          clippy
+        ];
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
         shellHook = ''
-          echo "Welcome to your C development shell with python3 plotting!"
+          echo "Welcome to your Rust development shell with Python support!"
           exec ${myShell}
         '';
       };
